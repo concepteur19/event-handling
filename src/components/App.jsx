1869ddc9872249d797247153ee220db35017f150
concepteur19 <zobel.tchomgui@gmail.com>
2022-12-14 06:12:53 +0100

    EVENT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="container">
       <h1> {headingText} </h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseDown}>Submit</button>
     </div>
   );
 }
