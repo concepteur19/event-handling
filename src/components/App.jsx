180b3f8b58b2375c79d2db3d0be925165f3863d7
concepteur19 <zobel.tchomgui@gmail.com>
2022-12-14 06:10:36 +0100

    EVENTS

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  return (
+    <div className="container">
+      <h1> {headingText} </h1>
+      <input type="text" placeholder="What's your name?" />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
