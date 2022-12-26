36476ee1129d4f08f91486820705c457a32e9bfa
concepteur19 <zobel.tchomgui@gmail.com>
2022-12-26 09:24:52 +0100

    26-12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,99 @@
+// import React, {useState} from "react";
+
+// function App() {
+//   const [headingText, setHeadingText] = useState("");
+//   const [isMouseOver, setMouseOver] = useState(false)
+//   const [name, setName] = useState("")
+  
+//   function handleClick(event) {
+//     setHeadingText(name);
+
+//     event.preventDefault();
+//   }; 
+
+
+//   function handleMouseOver() {
+//     setMouseOver(true);
+//   }
+
+//   function handleMouseDown() {
+//     setMouseOver(false);
+//   }
+
+//   function handleChange(event) {
+//     setName(event.target.value);
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1> Hello {headingText}! </h1>
+//       <form onSubmit={handleClick}>
+//         <input
+//           onChange={handleChange} 
+//           type="text" 
+//           placeholder="What's your name?" 
+//           //value={name}
+//         />
+//         <button style={{backgroundColor: isMouseOver? "black" : "white"}} 
+//           onMouseOver={handleMouseOver} 
+//           onMouseOut={handleMouseDown}>Submit
+//         </button>
+//       </form>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, {useState} from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello");
-  
-  function handleClick() {
-    setHeadingText("Submitted");
+
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    // const inputName = event.target.name;
+    // const newValue = event.target.value;
+
+    const [value, name] = event.target;
+
+    setFullName(previous => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: previous.lName
+        }
+      } else if (name === "lName") {
+        return {
+          fName: previous.fName,
+          lName: value
+        }
+      }
+    })
   }
 
   return (
     <div className="container">
-      <h1> {headingText} </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseDown}>Submit</button>
+      <h1>Hello {fullName.fName} {fullName.lName} </h1>
+      <form>
+        <input 
+          onChange={handleChange} 
+          name="fName" 
+          placeholder="First Name" 
+          value={fullName.fName}  
+        />
+        <input 
+          onChange={handleChange} 
+          name="lName" 
+          placeholder="Last Name" 
+          value={fullName.lName} 
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
